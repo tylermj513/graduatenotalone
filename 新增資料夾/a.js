fbe0d888e5f41a9e109a8281baf8d792c1a4e8b0
tylermj <ben20415@gmail.com>
2024-03-21 09:12:08 +0800

    update 隔兩個禮拜再次更新 心情轉變好多qq

---
+++ 新增資料夾/a.js
@@ -0,0 +1,8 @@
+
+let cursor = document.getElementById("myCursor");
+window.addEventListener("mousemove",function(e){
+  let x = e.clientX;
+  let y = e.clientY;
+  cursor.style.left = x-25+"px";
+  cursor.style.top = y-25+"px";
+})
