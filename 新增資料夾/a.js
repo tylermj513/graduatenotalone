aca44a944a9e9ffb5314f476555b0dfebfb73a98
tylermj <ben20415@gmail.com>
2024-04-02 11:44:24 +0800

    2 april last one truly

--- 新增資料夾/a.js
+++
@@ -1,8 +0,0 @@
-
-let cursor = document.getElementById("myCursor");
-window.addEventListener("mousemove",function(e){
-  let x = e.clientX;
-  let y = e.clientY;
-  cursor.style.left = x-25+"px";
-  cursor.style.top = y-25+"px";
-})
